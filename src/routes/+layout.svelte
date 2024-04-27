aa20a25418b0feee41f8b2c2b7b112307f489b49
Super515 <smartmydevworld@gmail.com>
2024-04-27 01:33:36 -0700

    firstcommit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  import "../app.css";
+</script>
+
+<nav>
+	<a href="/">home</a>
+	<a href="/about">about</a>
+</nav>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1 class="text-3xl font-bold underline">
+  Home
+</h1>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
